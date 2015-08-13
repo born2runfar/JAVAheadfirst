988a4e42e9b4bb39225945c1a534919deb177294
Patrick <born2runfar@live.com>
2015-08-13 15:34:04 -0500

    commit

---
+++ Head First Java/src/TestTree.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class TestTree {
+	public static void main (String[] args) {
+		new TestTree().go();
+	}
+	
+	public void go() {
+		Book b1 = new Book("How Cats Work");
+		Book b2 = new Book("Remix your Body");
+		Book b3 = new Book("Finding Emo");
+		Book b4 = new Book("Finding Emo");
+		
+		TreeSet<Book> tree = new TreeSet<Book>();
+		tree.add(b1);
+		tree.add(b2);
+		tree.add(b3);
+		tree.add(b4);
+		System.out.println(tree);
+	}
+}
+
+class Book implements Comparable<Book> {
+	String title;
+	public Book(String t) {
+		title = t;
+	}
+	public int compareTo(Book x) {
+		return title.compareTo(x.title);
+	}
+	public String toString() {
+		return title;
+	}
+}
